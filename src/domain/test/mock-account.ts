416d05796ac0d4b7a937af2031c1e37812d40a27
Andre Sampaio <andresampaiolm@gmail.com>
2022-04-09 16:28:35 -0300

    refactor: group imports in index files

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,6 @@
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
 import { randEmail, randGitCommitSha, randPassword } from '@ngneat/falso'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-model'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: randEmail(),
